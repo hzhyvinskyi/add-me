f816a47cc7abe02bb27d34a67157686480323ffd
hzhyvinskyi <hzhyvinskyi@gmail.com>
2018-06-30 15:58:45 +0300

    Main layout

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -3,14 +3,15 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
+use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
+use frontend\assets\FontAwesomeAsset;
 use common\widgets\Alert;
 
 AppAsset::register($this);
+FontAwesomeAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -23,62 +24,79 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body>
+<body class="home page">
 <?php $this->beginBody() ?>
+	<div class="wrapper">
+		<header>
+			<div class="header-top">
+				<div class="container">
+					<div class="col-md-4 col-sm-4 col-md-offset-4 col-sm-offset-4 brand-logo">
+						<h1>
+							<a href="<?= Url::to(['/site/index']) ?>">
+								<img src="/img/logo.png" alt="Add me">
+							</a>
+						</h1>
+					</div>
+					<div class="col-md-4 col-sm-4 navicons-topbar">
+						<ul>
+							<li class="blog-search">
+								<a href="#" title="Search"><i class="fa fa-search"></i></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->params['appName'],
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/user/default/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/user/default/login']];
-    } else {
-    	$menuItems[] = ['label' => 'Create post', 'url' => ['/post/default/create']];
-    	$menuItems[] = ['label' => 'My profile', 'url' => ['/user/profile/view', 'nickname' => Yii::$app->user->identity->getNickname()]];
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/user/default/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
-
+			<div class="header-main-nav">
+				<div class="container">
+					<div class="main-nav-wrapper">
+						<nav class="main-menu">
+							<?php
+							$menuItems = [
+								['label' => 'Newsfeed', 'url' => ['/site/index']],
+								['label' => 'About', 'url' => ['/site/about']],
+								['label' => 'Contact', 'url' => ['/site/contact']],
+							];
+							if (Yii::$app->user->isGuest) {
+								$menuItems[] = ['label' => 'Signup', 'url' => ['/user/default/signup']];
+								$menuItems[] = ['label' => 'Login', 'url' => ['/user/default/login']];
+							} else {
+								$menuItems[] = ['label' => 'Create post', 'url' => ['/post/default/create']];
+								$menuItems[] = ['label' => 'My profile', 'url' => ['/user/profile/view', 'nickname' => Yii::$app->user->identity->getNickname()]];
+								$menuItems[] = '<li>'
+								. Html::beginForm(['/user/default/logout'], 'post')
+								. Html::submitButton(
+										'Logout (' . Yii::$app->user->identity->username . ') <i class="fa fa-sign-out"></i>',
+										['class' => 'btn btn-link logout']
+								)
+								. Html::endForm()
+								. '</li>';
+							}
+							echo Nav::widget([
+									'options' => ['class' => 'menu navbar-nav navbar-right'],
+								'items' => $menuItems,
+							]);
+							?>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="container full">
+			<?= Alert::widget() ?>
+			<?= $content ?>
+		</div>
+		<div class="push"></div>
+	</div>
+	<br><br>
+	<footer>
+		<div class="footer">
+			<div class="back-to-top-page">
+				<a class="back-to-top"><i class="fa fa-angle-double-up"></i></a>
+			</div>
+			<p class="text"><?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+		</div>
+	</footer>
 <?php $this->endBody() ?>
 </body>
 </html>
